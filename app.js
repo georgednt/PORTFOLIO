b5d1c4f1551316753bdc8ee71db9f79c7e289035
georgednt <deltalzk@gmail.com>
2024-10-28 15:34:49 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+// Smooth scroll effect for navigation
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        document.getElementById(targetId).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+/*animation show details*/
+function showDetails(projectId) {
+    const project = document.getElementById(projectId);
+    project.classList.toggle("show");
+}
+
+/*button switch*/
+const themeToggle = document.getElementById('theme-toggle');
+themeToggle.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+    themeToggle.textContent = document.body.classList.contains('dark-mode') ? 'Light Mode' : 'Dark Mode';
+});
