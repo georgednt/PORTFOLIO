4bd4371c0f074c00a1a45978e02c6d95ab24509a
George Dontas <deltalzk@gmail.com>
2024-11-03 21:40:47 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -9,13 +9,36 @@ document.querySelectorAll('nav a').forEach(anchor => {
     });
 });
 
-/*animation show details*/
+// Toggle project details
 function showDetails(projectId) {
     const project = document.getElementById(projectId);
     project.classList.toggle("show");
 }
 
-/*button switch*/
+// Zoom effect for image
+function zoomImage(img) {
+    const zoomedImage = document.createElement('div');
+    zoomedImage.style.backgroundImage = `url(${img.src})`;
+    zoomedImage.style.backgroundSize = 'contain';
+    zoomedImage.style.backgroundRepeat = 'no-repeat';
+    zoomedImage.style.backgroundPosition = 'center';
+    zoomedImage.style.position = 'fixed';
+    zoomedImage.style.top = '50%';
+    zoomedImage.style.left = '50%';
+    zoomedImage.style.width = '80%';
+    zoomedImage.style.height = '80%';
+    zoomedImage.style.transform = 'translate(-50%, -50%)';
+    zoomedImage.style.backgroundColor = 'rgba(0, 0, 0, 0.8)';
+    zoomedImage.style.cursor = 'zoom-out';
+    zoomedImage.style.zIndex = '1000';
+    
+    zoomedImage.onclick = function () {
+        document.body.removeChild(zoomedImage);
+    };
+    document.body.appendChild(zoomedImage);
+}
+
+// Toggle theme between Dark Mode and Light Mode
 const themeToggle = document.getElementById('theme-toggle');
 themeToggle.addEventListener('click', () => {
     document.body.classList.toggle('dark-mode');
